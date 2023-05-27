2fb18602ebee5ce77454b45351bf99c6e1321d2b
IssadaornNK <49282901+IssadaornNK@users.noreply.github.com>
2023-05-28 06:14:03 +0700

    a

--- Client/src/components/HomeComponents/H_Pollution.jsx
+++ Client/src/components/HomeComponents/H_Pollution.jsx
@@ -14,14 +14,14 @@ const H_Pollution = () => {
       }}
     >
       {/* Container */}
-      <div className="px-8 items-center flex flex-col justify-center">
-        <div className="grid sm:grid-cols-2 gap-24 px-64 my-56">
-          <h1 className="text-[400px] font-bold text-red-500 items-center">
+      <div className=" items-center flex flex-col justify-center">
+        <div className="grid lg:grid-cols-2 lg:gap-24 md:gap-0 sm:gap-18 lg:px-64 my-56 ">
+          <h1 className="lg:text-[300px] md:text-[220px] sm:text-[200px] xs:text-[64px] font-bold text-red-500 items-center ">
             40%
           </h1>
-          <div className="justify-center items-start flex flex-col">
-            <h1 className="text-[150px] font-bold text-[#fff]">pollution</h1>
-            <h1 className="text-[150px] font-bold text-[#fff]">reduce</h1>
+          <div className="justify-center items-start flex flex-col ">
+            <h1 className="lg:text-[150px] md:text-[80px] sm:text-[75px] xs:text-[48px] font-bold text-[#fff] ">pollution</h1>
+            <h1 className="lg:text-[150px] md:text-[80px] sm:text-[75px] xs:text-[48px] font-bold text-[#fff] ">reduce</h1>
           </div>
         </div>
       </div>
